e49b75bf00042850f0456c7e4ea650620599dc1a
aguacateToledo <88992585+aguacateToledo@users.noreply.github.com>
2021-08-21 23:24:05 -0600

    Update

---
+++ 201800484_CUBRECAMAS/201800484_CUBRECAMAS/Form1.Designer.vb
@@ -0,0 +1,372 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtLino = New System.Windows.Forms.TextBox()
+        Me.txtAlgodon = New System.Windows.Forms.TextBox()
+        Me.txtSeda = New System.Windows.Forms.TextBox()
+        Me.txtHiloCrudo = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtTotalHiloCrudo = New System.Windows.Forms.TextBox()
+        Me.txtTotalSeda = New System.Windows.Forms.TextBox()
+        Me.txtTotalAlgodon = New System.Windows.Forms.TextBox()
+        Me.txtTotalLino = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtPrecioCosto = New System.Windows.Forms.TextBox()
+        Me.txtPrecioVenta = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.RadioButton4)
+        Me.GroupBox3.Controls.Add(Me.RadioButton3)
+        Me.GroupBox3.Controls.Add(Me.RadioButton2)
+        Me.GroupBox3.Controls.Add(Me.RadioButton1)
+        Me.GroupBox3.Location = New System.Drawing.Point(23, 40)
+        Me.GroupBox3.Margin = New System.Windows.Forms.Padding(4)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Padding = New System.Windows.Forms.Padding(4)
+        Me.GroupBox3.Size = New System.Drawing.Size(199, 130)
+        Me.GroupBox3.TabIndex = 8
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Mano de obra"
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(9, 98)
+        Me.RadioButton4.Margin = New System.Windows.Forms.Padding(4)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(62, 21)
+        Me.RadioButton4.TabIndex = 3
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "KING"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(9, 75)
+        Me.RadioButton3.Margin = New System.Windows.Forms.Padding(4)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(78, 21)
+        Me.RadioButton3.TabIndex = 2
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "QUEEN"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(8, 50)
+        Me.RadioButton2.Margin = New System.Windows.Forms.Padding(4)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(123, 21)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "MATRIMONIAL"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(8, 22)
+        Me.RadioButton1.Margin = New System.Windows.Forms.Padding(4)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(91, 21)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "IMPERIAL"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(285, 62)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(62, 21)
+        Me.CheckBox1.TabIndex = 9
+        Me.CheckBox1.Text = "LINO"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(285, 91)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(100, 21)
+        Me.CheckBox2.TabIndex = 10
+        Me.CheckBox2.Text = "ALGODON"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Location = New System.Drawing.Point(285, 147)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(116, 21)
+        Me.CheckBox4.TabIndex = 12
+        Me.CheckBox4.Text = "HILO CRUDO"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(285, 118)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(67, 21)
+        Me.CheckBox3.TabIndex = 11
+        Me.CheckBox3.Text = "SEDA"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(285, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 17)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "Material"
+        '
+        'txtLino
+        '
+        Me.txtLino.Location = New System.Drawing.Point(419, 62)
+        Me.txtLino.Name = "txtLino"
+        Me.txtLino.Size = New System.Drawing.Size(100, 22)
+        Me.txtLino.TabIndex = 14
+        Me.txtLino.Visible = False
+        '
+        'txtAlgodon
+        '
+        Me.txtAlgodon.Location = New System.Drawing.Point(419, 90)
+        Me.txtAlgodon.Name = "txtAlgodon"
+        Me.txtAlgodon.Size = New System.Drawing.Size(100, 22)
+        Me.txtAlgodon.TabIndex = 15
+        Me.txtAlgodon.Visible = False
+        '
+        'txtSeda
+        '
+        Me.txtSeda.Location = New System.Drawing.Point(419, 118)
+        Me.txtSeda.Name = "txtSeda"
+        Me.txtSeda.Size = New System.Drawing.Size(100, 22)
+        Me.txtSeda.TabIndex = 16
+        Me.txtSeda.Visible = False
+        '
+        'txtHiloCrudo
+        '
+        Me.txtHiloCrudo.Location = New System.Drawing.Point(419, 145)
+        Me.txtHiloCrudo.Name = "txtHiloCrudo"
+        Me.txtHiloCrudo.Size = New System.Drawing.Size(100, 22)
+        Me.txtHiloCrudo.TabIndex = 17
+        Me.txtHiloCrudo.Visible = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(288, 209)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 18
+        Me.Button1.Text = "Calcular"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(23, 286)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(104, 17)
+        Me.Label2.TabIndex = 19
+        Me.Label2.Text = "Precios Finales"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(23, 318)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(106, 17)
+        Me.Label3.TabIndex = 20
+        Me.Label3.Text = "Precio de costo"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(20, 348)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(107, 17)
+        Me.Label4.TabIndex = 21
+        Me.Label4.Text = "Precio de venta"
+        '
+        'txtTotalHiloCrudo
+        '
+        Me.txtTotalHiloCrudo.Location = New System.Drawing.Point(540, 145)
+        Me.txtTotalHiloCrudo.Name = "txtTotalHiloCrudo"
+        Me.txtTotalHiloCrudo.Size = New System.Drawing.Size(100, 22)
+        Me.txtTotalHiloCrudo.TabIndex = 25
+        Me.txtTotalHiloCrudo.Visible = False
+        '
+        'txtTotalSeda
+        '
+        Me.txtTotalSeda.Location = New System.Drawing.Point(540, 118)
+        Me.txtTotalSeda.Name = "txtTotalSeda"
+        Me.txtTotalSeda.Size = New System.Drawing.Size(100, 22)
+        Me.txtTotalSeda.TabIndex = 24
+        Me.txtTotalSeda.Visible = False
+        '
+        'txtTotalAlgodon
+        '
+        Me.txtTotalAlgodon.Location = New System.Drawing.Point(540, 90)
+        Me.txtTotalAlgodon.Name = "txtTotalAlgodon"
+        Me.txtTotalAlgodon.Size = New System.Drawing.Size(100, 22)
+        Me.txtTotalAlgodon.TabIndex = 23
+        Me.txtTotalAlgodon.Visible = False
+        '
+        'txtTotalLino
+        '
+        Me.txtTotalLino.Location = New System.Drawing.Point(540, 62)
+        Me.txtTotalLino.Name = "txtTotalLino"
+        Me.txtTotalLino.Size = New System.Drawing.Size(100, 22)
+        Me.txtTotalLino.TabIndex = 22
+        Me.txtTotalLino.Visible = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(537, 39)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(40, 17)
+        Me.Label5.TabIndex = 26
+        Me.Label5.Text = "Total"
+        '
+        'txtPrecioCosto
+        '
+        Me.txtPrecioCosto.Location = New System.Drawing.Point(173, 318)
+        Me.txtPrecioCosto.Name = "txtPrecioCosto"
+        Me.txtPrecioCosto.Size = New System.Drawing.Size(100, 22)
+        Me.txtPrecioCosto.TabIndex = 27
+        '
+        'txtPrecioVenta
+        '
+        Me.txtPrecioVenta.Location = New System.Drawing.Point(173, 346)
+        Me.txtPrecioVenta.Name = "txtPrecioVenta"
+        Me.txtPrecioVenta.Size = New System.Drawing.Size(100, 22)
+        Me.txtPrecioVenta.TabIndex = 28
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(188, 209)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 29
+        Me.Button2.Text = "Limpiar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(385, 209)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 30
+        Me.Button3.Text = "Salir"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.txtPrecioVenta)
+        Me.Controls.Add(Me.txtPrecioCosto)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtTotalHiloCrudo)
+        Me.Controls.Add(Me.txtTotalSeda)
+        Me.Controls.Add(Me.txtTotalAlgodon)
+        Me.Controls.Add(Me.txtTotalLino)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtHiloCrudo)
+        Me.Controls.Add(Me.txtSeda)
+        Me.Controls.Add(Me.txtAlgodon)
+        Me.Controls.Add(Me.txtLino)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CheckBox4)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtLino As TextBox
+    Friend WithEvents txtAlgodon As TextBox
+    Friend WithEvents txtSeda As TextBox
+    Friend WithEvents txtHiloCrudo As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtTotalHiloCrudo As TextBox
+    Friend WithEvents txtTotalSeda As TextBox
+    Friend WithEvents txtTotalAlgodon As TextBox
+    Friend WithEvents txtTotalLino As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtPrecioCosto As TextBox
+    Friend WithEvents txtPrecioVenta As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ 201800484_CUBRECAMAS/201800484_CUBRECAMAS/Form1.vb
@@ -0,0 +1,200 @@
+﻿Public Class Form1
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If (CheckBox1.Checked) Then
+            txtLino.Visible = True
+            txtLino.Focus()
+        Else
+            txtLino.Clear()
+            txtLino.Visible = False
+        End If
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        If (CheckBox2.Checked) Then
+            txtAlgodon.Visible = True
+            txtAlgodon.Focus()
+        Else
+            txtAlgodon.Clear()
+            txtAlgodon.Visible = False
+        End If
+    End Sub
+
+    Private Sub CheckBox4_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
+        If (CheckBox3.Checked) Then
+            txtSeda.Visible = True
+            txtSeda.Focus()
+        Else
+            txtSeda.Clear()
+            txtSeda.Visible = False
+        End If
+    End Sub
+
+    Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox4.CheckedChanged
+        If (CheckBox4.Checked) Then
+            txtHiloCrudo.Visible = True
+            txtHiloCrudo.Focus()
+        Else
+            txtHiloCrudo.Clear()
+            txtHiloCrudo.Visible = False
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim cantLino As Double = 0
+        Dim cantAlgodon As Double = 0
+        Dim cantSeda As Double = 0
+        Dim cantHiloCrudo As Double = 0
+        Dim cantTotalYarda As Double = 0
+        Dim precio As Double = 0
+        Dim precioVenta As Double = 0
+        Dim necYardas As Double = 3
+        Dim necMatrimonial As Double = 5
+        Dim necQueen As Double = 6
+        Dim necKing As Double = 7
+
+        If RadioButton1.Checked Or RadioButton2.Checked Or RadioButton3.Checked Or RadioButton4.Checked Then
+
+        Else
+            MsgBox("Seleccione una mano de obra")
+            Return
+        End If
+
+        If ((CheckBox1.Checked) Or (CheckBox2.Checked) Or (CheckBox4.Checked) Or (CheckBox3.Checked)) Then
+            If (CheckBox1.Checked) Then
+                If (IsNumeric(txtLino.Text) And Val(txtLino.Text) > 0) Then
+                    cantLino = Val(txtLino.Text)
+                    precio += cantLino * 15
+                    txtTotalLino.Visible = True
+                    txtTotalLino.Text = cantLino * 15
+                Else
+                    MsgBox("ERROR, DATO DE LINO NO VÁLIDO")
+                    txtLino.Focus()
+                    Exit Sub
+                End If
+            Else
+                cantLino = 0
+            End If
+
+            If (CheckBox2.Checked) Then
+                If (IsNumeric(txtAlgodon.Text) And Val(txtAlgodon.Text) > 0) Then
+                    cantAlgodon += Val(txtAlgodon.Text)
+                    precio += cantAlgodon * 23.99
+                    txtTotalAlgodon.Visible = True
+                    txtTotalAlgodon.Text = cantAlgodon * 23.99
+                Else
+                    MsgBox("ERROR, DATO DE ALGODON NO VÁLIDO")
+                    txtAlgodon.Focus()
+                    Exit Sub
+                End If
+            Else
+                cantAlgodon = 0
+            End If
+
+            If (CheckBox3.Checked) Then
+                If (IsNumeric(txtSeda.Text) And Val(txtSeda.Text) > 0) Then
+                    cantSeda += Val(txtSeda.Text)
+                    precio = cantSeda * 30.99
+                    txtTotalSeda.Visible = True
+                    txtTotalSeda.Text = cantSeda * 30.99
+                Else
+                    MsgBox("ERROR, DATO DE SEDA NO VÁLIDO")
+                    txtSeda.Focus()
+                    Exit Sub
+                End If
+            Else
+                cantSeda = 0
+            End If
+
+            If (CheckBox4.Checked) Then
+                If (IsNumeric(txtHiloCrudo.Text) And Val(txtHiloCrudo.Text) > 0) Then
+                    cantHiloCrudo = Val(txtHiloCrudo.Text)
+                    precio += cantHiloCrudo * 39.99
+                    txtTotalHiloCrudo.Visible = True
+                    txtTotalHiloCrudo.Text = cantHiloCrudo * 39.99
+                Else
+                    MsgBox("ERROR, DATO DE HILO CRUDO NO VÁLIDO")
+                    txtHiloCrudo.Focus()
+                    Exit Sub
+                End If
+            Else
+                cantHiloCrudo = 0
+            End If
+
+            cantTotalYarda = cantAlgodon + cantHiloCrudo + cantLino + cantSeda
+
+            If RadioButton1.Checked Then
+                If cantTotalYarda <= 3 Then
+                    precio = precio + 65.5
+                Else
+                    MsgBox("la cantidad de yardas para la imperial sobre pasa la cantidad para elaborarla ")
+                    Return
+                End If
+            End If
+
+            If RadioButton2.Checked Then
+                If cantTotalYarda <= 5 Then
+                    precio = precio + 85.99
+                Else
+                    MsgBox("la cantidad de yardas para la matrimonial sobre pasa la cantidad para elaborarla ")
+                    Return
+                End If
+            End If
+
+            If RadioButton3.Checked Then
+                If cantTotalYarda <= 6 Then
+                    precio = precio + 99.99
+                Else
+                    MsgBox("la cantidad de yardas para la Queen sobre pasa la cantidad para elaborarla ")
+                    Return
+                End If
+            End If
+
+            If RadioButton4.Checked Then
+                If cantTotalYarda <= 7 Then
+                    precio = precio + 105.99
+                Else
+                    MsgBox("la cantidad de yardas para la King sobre pasa la cantidad para elaborarla ")
+                    Return
+                End If
+            End If
+
+            precioVenta = precio + (precio * 0.65)
+            txtPrecioCosto.Text = precio
+            txtPrecioVenta.Text = precioVenta
+        Else
+            MsgBox("Seleccione un material")
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        txtAlgodon.Text = ""
+        txtAlgodon.Visible = False
+        txtHiloCrudo.Text = ""
+        txtHiloCrudo.Visible = False
+        txtLino.Text = ""
+        txtLino.Visible = False
+        txtPrecioCosto.Text = ""
+        txtPrecioVenta.Text = ""
+        txtSeda.Text = ""
+        txtSeda.Visible = False
+        txtTotalAlgodon.Text = ""
+        txtTotalAlgodon.Visible = False
+        txtTotalHiloCrudo.Text = ""
+        txtTotalHiloCrudo.Visible = False
+        txtTotalLino.Text = ""
+        txtTotalLino.Visible = False
+        txtTotalSeda.Text = ""
+        txtTotalSeda.Visible = False
+        CheckBox1.Checked = False
+        CheckBox2.Checked = False
+        CheckBox3.Checked = False
+        CheckBox4.Checked = False
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If MsgBox("¿DESEA SALIR?", vbQuestion + vbYesNo, "salir") = vbYes Then
+            Me.Close()
+        End If
+    End Sub
+End Class
